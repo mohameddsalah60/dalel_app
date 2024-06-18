b1a2a223c6f524dec8c0f4bcd3058a7ffde1c419
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2024-06-19 02:20:57 +0300

    add images & create assets file

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,36 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesForgotPassword
+  /// assets/images/forgot_password.svg
+  static const String imagesForgotPassword = "assets/images/forgot_password.svg";
+
+  /// Assets for imagesFrame1
+  /// assets/images/frame_1.svg
+  static const String imagesFrame1 = "assets/images/frame_1.svg";
+
+  /// Assets for imagesFrame2
+  /// assets/images/frame_2.svg
+  static const String imagesFrame2 = "assets/images/frame_2.svg";
+
+  /// Assets for imagesFrame3
+  /// assets/images/frame_3.svg
+  static const String imagesFrame3 = "assets/images/frame_3.svg";
+
+  /// Assets for imagesOrderSuccess
+  /// assets/images/order_success.svg
+  static const String imagesOrderSuccess = "assets/images/order_success.svg";
+
+  /// Assets for imagesVector1
+  /// assets/images/vector1.svg
+  static const String imagesVector1 = "assets/images/vector1.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/vector2.svg
+  static const String imagesVector2 = "assets/images/vector2.svg";
+
+  /// Assets for imagesVector3
+  /// assets/images/vector3.svg
+  static const String imagesVector3 = "assets/images/vector3.svg";
+}
+
