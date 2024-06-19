d8d7074574c17b94d91232ca1717fc28b796ad6f
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2024-06-19 03:00:27 +0300

    splash view done & create on boarding view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,13 +1,18 @@
+import 'package:dalel_app/features/onBoarding/presentation/views/on_boarding_view.dart';
 import 'package:dalel_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
+  static const kOnBoarding = '/onBoarding';
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => const SplashView(),
       ),
+      GoRoute(
+          path: kOnBoarding,
+          builder: (context, state) => const OnBoardingView()),
     ],
   );
 }
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,27 @@
+import 'package:dalel_app/core/utils/app_router.dart';
 import 'package:dalel_app/core/utils/app_strings.dart';
 import 'package:dalel_app/core/utils/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        GoRouter.of(context).push(AppRouter.kOnBoarding);
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
